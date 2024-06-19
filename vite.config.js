36408a9ac5932b3209848c59158a1568585f74fa
张嘉豪 <zhangjiahao@aoemo.com>
2024-06-19 18:10:01 +0800

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CoinWise/', // 将 'my-vite-project' 替换为你的仓库名
   plugins: [vue()],
   resolve: {
     alias: {
